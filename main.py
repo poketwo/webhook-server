15c48f8c6697f59cff8aaf350e8b6b37c70bfb18
Oliver Ni <oliver.ni@gmail.com>
2021-11-25 14:02:00 -0800

    Add fundraiser webhook

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import pickle
+import random
 from datetime import datetime, timedelta
 from urllib.parse import quote_plus
 
@@ -12,13 +13,44 @@ from starlette.responses import PlainTextResponse
 
 # Constants
 
-PRODUCTS = {
+SHARD_PRODUCT_IDS = {
     "prod_I6mX11xoeVPuE6": 500,
     "prod_I6mXA1Bq9xI52b": 1100,
     "prod_I6mX6x18dVqr9D": 2400,
     "prod_I6mXe6caYABny8": 5600,
     "prod_Ja5G3l3d2MnMUU": 15000,
 }
+
+MSF_FUNDRAISER_PRODUCT_IDS = ["prod_Kf94LTHSJjqsrP", "prod_Kf94xExNn3icbL"]
+
+
+NATURES = [
+    "Adamant",
+    "Bashful",
+    "Bold",
+    "Brave",
+    "Calm",
+    "Careful",
+    "Docile",
+    "Gentle",
+    "Hardy",
+    "Hasty",
+    "Impish",
+    "Jolly",
+    "Lax",
+    "Lonely",
+    "Mild",
+    "Modest",
+    "Naive",
+    "Naughty",
+    "Quiet",
+    "Quirky",
+    "Rash",
+    "Relaxed",
+    "Sassy",
+    "Serious",
+    "Timid",
+]
 
 stripe.api_key = os.environ["STRIPE_KEY"]
 STRIPE_SECRET = os.environ["STRIPE_SECRET"]
@@ -44,7 +76,8 @@ REDIS_CONF = {
 # Setup
 
 redis = None
-db = AsyncIOMotorClient(DATABASE_URI)[DATABASE_NAME]
+client = AsyncIOMotorClient(DATABASE_URI)
+db = client[DATABASE_NAME]
 app = Starlette(debug=os.getenv("DEBUG", False))
 
 
@@ -76,11 +109,6 @@ async def dbl(request):
         return PlainTextResponse("Invalid User", 404)
 
     streak = res.get("vote_streak", 0)
-
-    # last_voted = res.get("last_voted", datetime.min)
-    # if now - last_voted > timedelta(days=2):
-    #     streak = 0
-
     streak += 1
 
     if streak >= 40 and streak % 10 == 0:
@@ -100,10 +128,6 @@ async def dbl(request):
         },
     )
     await redis.hdel(f"db:member", uid)
-
-    article = "an" if box_type == "ultra" else "a"
-    text = f"Thanks for voting! You received {article} **{box_type} box**."
-    # await redis.rpush("send_dm", pickle.dumps((uid, text)))
 
     return PlainTextResponse("Success")
 
@@ -133,15 +157,73 @@ async def stripe_webhook(request):
     item = await loop.run_in_executor(None, get_checkout_item, session.id)
 
     uid = int(session.metadata.id)
-    shards = PRODUCTS[item.price.product]
 
-    await db.member.update_one({"_id": uid}, {"$inc": {"premium_balance": shards}})
-    await redis.hdel(f"db:member", uid)
-    await redis.rpush(
-        "send_dm", pickle.dumps((uid, f"Thanks for donating! You received **{shards}** shards."))
+    if shards := SHARD_PRODUCT_IDS.get(item.price.product):
+        await db.member.update_one({"_id": uid}, {"$inc": {"premium_balance": shards}})
+        await redis.hdel(f"db:member", uid)
+        await redis.rpush(
+            "send_dm",
+            pickle.dumps((uid, f"Thanks for donating! You received **{shards}** shards.")),
+        )
+
+        return PlainTextResponse("Success")
+
+    elif item.price.product in MSF_FUNDRAISER_PRODUCT_IDS:
+        await client.support.fundraiser.update_one(
+            {"_id": "msf_2021"}, {"$inc": {"value": item.price.unit_amount}}, upsert=True
+        )
+        await db.member.update_one(
+            {"_id": uid}, {"$inc": {"msf_donated_amount": item.price.unit_amount}}
+        )
+
+        pokemon = []
+        for _ in range(item.price.unit_amount // 1500):
+            shiny = random.randint(1, 4096) == 1
+            level = min(max(int(random.normalvariate(20, 10)), 1), 100)
+            ivs = [random.randint(0, 31) for _ in range(6)]
+            pokemon.append(
+                {
+                    "owner_id": uid,
+                    "species_id": 50032,
+                    "level": level,
+                    "xp": 0,
+                    "nature": random.choice(NATURES),
+                    "iv_hp": ivs[0],
+                    "iv_atk": ivs[1],
+                    "iv_defn": ivs[2],
+                    "iv_satk": ivs[3],
+                    "iv_sdef": ivs[4],
+                    "iv_spd": ivs[5],
+                    "iv_total": sum(ivs),
+                    "moves": [],
+                    "shiny": shiny,
+                    "idx": await fetch_next_idx(uid),
+                }
+            )
+
+        if len(pokemon) > 0:
+            await db.pokemon.insert_many(pokemon)
+
+        msg = f"Thanks for contributing **${item.price.unit_amount / 100:.2f}** to our Thanksgiving fundraiser for Médecins Sans Frontières / Doctors Without Borders! Your charitable donation is greatly appreciated. 100% of your donation will be donated to the charity, and Pokétwo will cover all transaction fees associated with the payment."
+
+        if len(pokemon) > 0:
+            msg += f" You have received **{len(pokemon)} United Pikachu{'s' if len(pokemon) > 1 else ''}**."
+
+        await redis.rpush("send_dm", pickle.dumps((uid, msg)))
+
+        return PlainTextResponse("Success")
+
+    return PlainTextResponse("Error", 400)
+
+
+async def fetch_next_idx(uid, reserve=1):
+    result = await db.member.find_one_and_update(
+        {"_id": uid},
+        {"$inc": {"next_idx": reserve}},
+        projection={"next_idx": 1},
     )
-
-    return PlainTextResponse("Success")
+    await redis.hdel(f"db:member", uid)
+    return result["next_idx"]
 
 
 @app.route("/captcha", methods=["POST"])
@@ -152,7 +234,6 @@ async def captcha_webhook(request):
         return PlainTextResponse("Invalid Secret", 401)
 
     uid = int(data["uid"])
-    print(uid)
 
     await redis.hdel("captcha", uid)
     return PlainTextResponse("Success")
